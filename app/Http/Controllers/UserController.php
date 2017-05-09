07e9e0ac964ad7440e42afcbaeb829d7313cfb41
Velizar Ivanov <zivanof@gmail.com>
2017-05-10 00:03:51 +0300

    changed user retrievel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends BaseController {
 
-    public function getUser() {
-        $user = \App\User::getUserData(1);
+    public function getUser(Request $request) {
+        if (Auth::guard()->check()) {
+            $user = User::getUserData($request->user()->id);
+        } else {
+            $user = [];
+        }
 
         return response($user)
             ->header('Content-Type', 'application/json');
